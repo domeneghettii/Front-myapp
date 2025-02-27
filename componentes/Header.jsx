becc08a85aef06694a581516de75e21b5c00ee3d
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-02-27 07:51:03 -0300

    feat: criação de um novo repositório.

---
+++ componentes/Header.jsx
@@ -0,0 +1,10 @@
+import styles from "../styles/Header.jsx"
+
+export default function Header() {
+    return (
+        <header className={styles.header}>
+                <h1 className={styles.title}>3TDS1 - Luana Domeneghetti ✨</h1>
+            </header>
+    )
+
+}
---
+++ pages/_app.jsx
@@ -0,0 +1,5 @@
+import '../styles/globals.css';
+
+export default function MyApp({Component, pagesProps }) {
+    return <Component {...pagesProps} />;
+}
