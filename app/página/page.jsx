f7954ab80de65bfa0bafb1fa729974fa44a1bf89
Luana Domeneghetti <158210063+domeneghettii@users.noreply.github.com>
2025-03-20 08:41:24 -0300

    feat: update

--- app/página/page.jsx
+++ app/página/page.jsx
@@ -1,6 +1,6 @@
-import Header from "../componentes/Header";
-import Footer from "../componentes/Footer";
-import Card from "../componentes/Card";
+import Header from "../../componentes/Header";
+import Footer from "../../componentes/Footer";
+import Card from "../../componentes/Card";
 
 export default function Home() {
     return (
